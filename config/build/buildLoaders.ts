85a115444ebca047f038457ce976116efd82893e
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2024-03-15 12:38:45 +0300

    Урок 13: Svg loader
    * добавил loader для работы с png, svg, jpg...
    
    * добавил компонент с кнопкой, использовал в навбаре(themeSwitcher)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,46 +1,51 @@
-import webpack from 'webpack'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { BuildOptions } from './types/config'
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders ({isDev}: BuildOptions): webpack.RuleSetRule[] {
-  
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
+ // можно добавить также шрифты  
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
+
   const scssLoader = {
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-        // Translates CSS into CommonJS
-        {
-          loader: "css-loader",
-          options: {
-            modules: 
-            {
-              auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-              localIdentName: isDev 
-                ? '[path][name]__[local]' 
-                : '[hash:base64:8]'
-            },
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
 
-
-            sourceMap: true
-          }
+          sourceMap: true,
         },
-        // Compiles Sass to CSS
-        "sass-loader",
-      ],
-  }
-  
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
   // Если не используем TS - нужен babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: 'ts-loader',
+    use: "ts-loader",
     exclude: /node_modules/,
-  }
+  };
 
-
-
-  return [
-    typescriptLoader,
-    scssLoader
-  ]
+  return [typescriptLoader, scssLoader, svgLoader, fileLoader];
 }
