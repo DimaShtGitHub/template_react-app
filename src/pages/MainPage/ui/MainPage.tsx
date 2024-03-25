db96e28fb8409f41a666c64a7260d881976e7c74
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2024-03-25 17:18:56 +0300

    Урок 15: языки на сайте
    * добавил возможность переключать языки на сайте
    
    * обновил сайдбар

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,9 @@
+import { useTranslation } from "react-i18next";
+
 const MainPage = () => {
-  return (
-    <>
-      <h1> Main </h1>
-    </>
-  );
+  const {t} = useTranslation('main');
+
+  return <div>{t("Главная страница")}</div>;
 };
 
 export default MainPage;
