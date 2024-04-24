e7ccfa8e2240b21b6c945ae17f54fc0cd4157ba9
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2024-04-24 11:11:39 +0300

    Урок 20: добавил Jest
    * добавил первый тест функции classNames
    
    * натсроил импорт для Jest с помощью babel

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,15 +3,15 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mods?: Mods,
-  additional?: string[],
+  mods: Mods,
+  additional: string[],
 ): string {
   return [
     cls,
     ...additional.filter(Boolean),
     // Object.entries это - obj = { foo: "bar", baz: 42 }; => Object.entries(obj) = [ ['foo', 'bar'], ['baz', 42] ]
-    Object.entries(mods)
-      .filter(([className, value]) => Boolean(value))
-      .map(([className, value]) => className),
+    ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
+      .map(([className]) => className),
   ].join(' ');
 }
