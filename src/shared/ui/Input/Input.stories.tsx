598f18ddb0f63d30bbb8a8eb53ffe7fb28f3c514
Shtankevich Dima <schtanckevitch.dima@yandex.ru>
2024-06-29 21:36:20 +0300

    * добавляет авторизацию внутрь модалки * создает кастомное поле ввода * в redux создает userSlice

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../../app/providers/ThemeProviders/index';
+import { Input } from './Input';
+
+export default {
+  title: 'widget/Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  placeholder: 'Enter text',
+  value: 'Text input',
+};
